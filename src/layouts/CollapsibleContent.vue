b8b1ba75193b01b8cb14f6504bd2b84021c41839
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-20 16:06:23 +0530

    add collapsible component

---
+++ src/layouts/CollapsibleContent.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(["isCollapsed"]);
+</script>
+
+<template>
+  <div class="overflow-hidden">
+    <div class="collapsible" :class="isCollapsed ? 'expand' : ''">
+      <div class="collapsible-content">
+        <slot name="collapsible-content"></slot>
+      </div>
+    </div>
+    <div class="sticky">
+      <slot name="sticky-content"></slot>
+    </div>
+  </div>
+</template>
