1e90f612578187a48c05bb247294055fbf40b147
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-20 13:17:51 +0530

    add base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      display: ["Roboto", "sans-serif"],
+      preview: ["Roboto Slab", "serif"],
+      mono: ["Roboto Mono", "monospace"],
+    },
+    colors: {
+      cream: "#F5F5F5",
+      white: "#FFFFFF",
+      black: {
+        600: "#35393F",
+        700: "#2B2D31",
+        800: "#1D1F22",
+        900: "#151619",
+      },
+      grey: {
+        600: "#E4E4E4",
+        700: "#C1C4CB",
+        800: "#7C8187",
+        900: "#5A6069",
+      },
+      orange: {
+        800: "#F39765",
+        900: "#E46643",
+      },
+    },
     extend: {},
   },
   plugins: [],
