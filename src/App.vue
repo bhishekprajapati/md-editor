14efe8cb556fc74266d1f41cdc590e9983addd2a
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-21 14:51:12 +0530

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,22 @@
 <script setup>
 import { ref } from "vue";
-import CollapsibleContent from "./layouts/CollapsibleContent.vue";
 import TheHeader from "./components/TheHeader.vue";
-import EditorMarkdown from "./components/EditorMarkdown.vue";
-import PreviewMarkdown from "./components/PreviewMarkdown.vue";
-import sampleMarkdownCode from "./sampleMarkdownCode";
-
+import TheMarkdown from "./components/TheMarkdown.vue";
+import CollapsibleContent from "./layouts/CollapsibleContent.vue";
 const isMenuCollapsed = ref(true);
-const markdownCode = ref(sampleMarkdownCode);
 </script>
 
 <template>
   <CollapsibleContent :is-collapsed="isMenuCollapsed">
     <template v-slot:collapsible-content></template>
 
-    <template v-slot:sticky-content>
+    <template v-slot:viewport>
       <TheHeader
         :is-menu-collapsed="isMenuCollapsed"
         @menu-toggle="() => (isMenuCollapsed = !isMenuCollapsed)" />
 
       <main class="h-[calc(100vh-56px)]">
-        <section class="flex h-full">
-          <div class="flex-1">
-            <div class="h-full w-full">
-              <EditorMarkdown
-                :value="markdownCode"
-                @input="(e) => (markdownCode = e.target.value)" />
-            </div>
-          </div>
-          <div
-            class="scrollbar-none flex-1 overflow-y-auto border-l-2 border-grey-600">
-            <PreviewMarkdown :code="markdownCode" />
-          </div>
-        </section>
+        <TheMarkdown />
       </main>
     </template>
   </CollapsibleContent>
---
+++ src/components/TheMarkdown.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+import TheMarkdownEditor from "./TheMarkdownEditor.vue";
+import TheMarkdownPreviewer from "./TheMarkdownPreviewer.vue";
+import sampleMarkdownCode from "../sampleMarkdownCode";
+
+const markdownCode = ref(sampleMarkdownCode);
+</script>
+
+<template>
+  <section class="flex h-full">
+    <div class="flex-1">
+      <div class="h-full w-full">
+        <TheMarkdownEditor
+          :value="markdownCode"
+          @input="(e) => (markdownCode = e.target.value)" />
+      </div>
+    </div>
+    <div
+      class="flex-1 overflow-y-auto border-l-2 border-grey-600 scrollbar-none">
+      <TheMarkdownPreviewer :code="markdownCode" />
+    </div>
+  </section>
+</template>
