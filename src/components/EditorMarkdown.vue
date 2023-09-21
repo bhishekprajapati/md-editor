7e8a2da28dc5557e07a91956d7ab31680de6a99b
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-21 13:23:46 +0530

    turn off spellchecker of the markdown editor

--- src/components/EditorMarkdown.vue
+++ src/components/EditorMarkdown.vue
@@ -1,4 +1,6 @@
 <template>
-  <textarea class="h-full w-full resize-none p-4 font-mono focus:outline-none">
+  <textarea
+    spellcheck="false"
+    class="h-full w-full resize-none p-4 font-mono focus:outline-none">
   </textarea>
 </template>
