eea76fdd7f1aa7cd02f2dcfe805a33e96b2ba0fe
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-22 17:49:02 +0530

    update

--- src/components/ButtonPrimary.vue
+++ src/components/ButtonPrimary.vue
@@ -1,13 +1,13 @@
 <template>
   <button
     type="button"
-    class="display-m rounded-md bg-orange-900 p-[0.62em] align-middle text-cream transition-all duration-500 hover:scale-105 hover:bg-orange-800 active:scale-95">
-    <span>
+    class="display-m rounded-md bg-orange-900 p-[0.62em] align-middle text-cream transition-all duration-500 hover:scale-105 hover:bg-orange-800 active:scale-95 disabled:pointer-events-none disabled:bg-grey-900">
+    <span class="pointer-events-none">
       <span>
         <slot name="icon"></slot>
       </span>
     </span>
-    <span>
+    <span class="pointer-events-none">
       <span>
         <slot></slot>
       </span>
