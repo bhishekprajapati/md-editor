3643a02d4bdf8e8773b8f43e0120d7558226c26c
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-23 21:04:33 +0530

    refactor

--- src/stores/useMarkdownStore.js
+++ src/stores/useMarkdownStore.js
@@ -14,6 +14,7 @@ export const useMarkdownStore = defineStore("markdowns", () => {
   const filename = ref(null);
   const fileContent = ref(null);
   const hasChanged = ref(false);
+  const fileList = ref(null);
 
   watch(filename, (current, old) => {
     if (!old) return;
@@ -35,34 +36,47 @@ export const useMarkdownStore = defineStore("markdowns", () => {
   });
 
   async function save() {
-    if (await activeFile.save()) hasChanged.value = false;
+    if (await activeFile.save()) {
+      hasChanged.value = false;
+
+      await refreshFileList();
+    }
+  }
+
+  async function refreshFileList() {
+    fileList.value = await MarkdownFile.getFileList();
+
+    // last edited first
+    fileList.value.sort(
+      (file1, file2) =>
+        new Date(file2.meta.createdAt) - new Date(file1.meta.createdAt),
+    );
   }
 
   async function populate() {
-    const storedFiles = await MarkdownFile.getFileList();
+    await refreshFileList();
 
-    if (!storedFiles.length) {
+    if (!fileList.value.length) {
       filename.value = SAMPLE_MD.filename;
       fileContent.value = SAMPLE_MD.content;
       activeFile = new MarkdownFile(SAMPLE_MD.filename, SAMPLE_MD.content);
     }
-    // load last edited
-    storedFiles.sort(
-      (file1, file2) =>
-        new Date(file2.meta.createdAt) - new Date(file1.meta.createdAt),
-    );
 
     // updating refs for loaded file
-    const lastEditedFile = await MarkdownFile.findOne(storedFiles[0].filename);
+    const lastEditedFile = await MarkdownFile.findOne(
+      fileList.value[0].filename,
+    );
     filename.value = lastEditedFile.filename;
     fileContent.value = lastEditedFile.content;
     activeFile = lastEditedFile;
   }
+
   return {
     filename,
     fileContent,
     hasChanged,
     save,
     populate,
+    fileList,
   };
 });
