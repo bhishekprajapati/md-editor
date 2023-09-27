516a7f2d6f2a57bca2ae75b8a1bc2f5d2713e2cd
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-09-27 11:20:39 +0530

    fix preview section bg color

--- src/components/TheMarkdown.vue
+++ src/components/TheMarkdown.vue
@@ -43,7 +43,7 @@ function togglePreview() {
           class="display-s absolute left-0 right-0 top-0 border-b-2 border-grey-600 px-4 py-2 font-medium leading-relaxed backdrop-blur-3xl dark:border-black-600 dark:text-grey-700">
           PREVIEW
         </div>
-        <TheMarkdownPreviewer :code="store.fileContent" />
+        <TheMarkdownPreviewer class="bg-white" :code="store.fileContent" />
       </div>
     </div>
   </section>
